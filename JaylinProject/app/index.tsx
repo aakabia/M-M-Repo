1791c5e4f1bbedb7dc0cd9b196cd5f15064e80c2
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-04-17 13:45:57 -0400

    added pdf as conditional on button press

--- JaylinProject/app/index.tsx
+++ JaylinProject/app/index.tsx
@@ -1,77 +1,141 @@
+import React, { useState } from "react";
 import { StyleSheet, Image, View, ScrollView } from "react-native";
 import MyButton from "../components/button";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { Link } from "expo-router";
+import * as Linking from "expo-linking";
 import { TouchableOpacity } from "react-native";
+import PDFVIEW from "../components/PDFVIEW";
+import { Asset } from "expo-asset";
+
+const styleGuide = require("../assets/pdfs/styleGuide.pdf");
+const venues = require("../assets/pdfs/venues.pdf");
+const whereToStayURl = "https://www.marketingandmimosas.com/fun-facts";
+// Above are our local pdf files and links for our buttons on the homepage 
+
+
 
 const HomeScreen = () => {
-  const onPress = () => {
-    console.log("Button Pressed");
+
+  const [pdfPath, setPdfPath] = useState<string | null>(null);
+  const [pdfShowing, setPdfShowing] = useState<boolean>(false);
+
+  // Above, we set two variables using state 
+  // pdfPath will dynamically choose which pdf we want to use 
+  // pdfShowing is a boolean to determine if a pdf is currently in view
+
+  const onPressWhereToStay = () => {
+    Linking.openURL(whereToStayURl);
   };
 
-  // Above will be the functionality for each button
+  // Above we use Linking from expo-linking to re route to a url of our choosing.
+  
+
+  const onPressVenues = () => {
+    const venuesAsset = Asset.fromModule(venues);
+    setPdfPath(venuesAsset.uri);
+    setPdfShowing(true);
+    //console.log(venuesAsset.uri);
+  };
+
+  // for our pdf files we use Asset.fromModule to create a object for our imported pdf file 
+  // this objects contains the uri of the pdf file which we can use for dynamic rendering
+  // we set the pdf path to the venues uri and set pdf showing to true
+  // this helps render between our hompage layout and pdf layout
+  // the same is done below in onPressStyleGuide 
+
+  const onPressStyleGuide = () => {
+    const styleGuideAsset = Asset.fromModule(styleGuide);
+    setPdfPath(styleGuideAsset.uri);
+    setPdfShowing(true);
+    //console.log(styleGuideAsset.uri);
+  };
 
   return (
-    <ScrollView style={styles.scrollViewContainer}>
-    <View style={styles.container}>
-      <View>
-        <Image
-          source={require("../assets/gifs/shoes.gif")}
-          style={styles.gifImage}
-          resizeMode="contain"
+    <>
+      {pdfShowing && pdfPath ? (
+        <PDFVIEW
+          filePath={pdfPath}
+          pdfShowing={pdfShowing}
+          setPdfShowing={setPdfShowing}
         />
-      </View>
+      ) : (
+        <ScrollView style={styles.scrollViewContainer}>
+          <View style={styles.container}>
+            <View>
+              <Image
+                source={require("../assets/gifs/shoes.gif")}
+                style={styles.gifImage}
+                resizeMode="contain"
+              />
+            </View>
 
-      <View style={styles.stickerContainer}>
-        <Image
-          resizeMode="contain"
-          style={styles.mmSticker}
-          source={require("../assets/images/mm2025sticker.png")}
-        />
+            <View style={styles.stickerContainer}>
+              <Image
+                resizeMode="contain"
+                style={styles.mmSticker}
+                source={require("../assets/images/mm2025sticker.png")}
+              />
 
-        <Image
-          style={styles.MMBanner}
-          source={require("../assets/images/MM2025BANNER.png")}
-        />
+              <Image
+                style={styles.MMBanner}
+                source={require("../assets/images/MM2025BANNER.png")}
+              />
 
-        <View style={styles.ButtonContainer}>
-          <MyButton text="SCHEDULE" onPress={onPress} />
-          <MyButton text="STYLE GUIDE" onPress={onPress} />
-          <MyButton text="WHERE TO STAY" onPress={onPress} />
-        </View>
-      </View>
+              <View style={styles.ButtonContainer}>
+                <MyButton
+                  text="SCHEDULE"
+                  onPress={() => console.log("button pressed")}
+                />
+                <MyButton text="STYLE GUIDE" onPress={onPressStyleGuide} />
+                <MyButton text="WHERE TO STAY" onPress={onPressWhereToStay} />
+                <MyButton text="OUR VENUES" onPress={onPressVenues} />
+              </View>
+            </View>
 
-      <Link asChild href={"https://www.instagram.com/marketingandmimosas_/"}>
-        <TouchableOpacity style={styles.IconContainer}>
-          <MaterialCommunityIcons name="instagram" size={35} color="white" />
-        </TouchableOpacity>
-      </Link>
-    </View>
-    </ScrollView>
+            <Link
+              asChild
+              href={"https://www.instagram.com/marketingandmimosas_/"}
+            >
+              <TouchableOpacity style={styles.IconContainer}>
+                <MaterialCommunityIcons
+                  name="instagram"
+                  size={35}
+                  color="white"
+                />
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </ScrollView>
+      )}
+    </>
   );
 };
 
 export default HomeScreen;
 
 // Above is our home page that includes recomended images from the client.
-// Also the page includes a  MyButton that will direct users to other pages.
-// Last, we use  MaterialCommunityIcons to import the required instagram photo image requested.
-
-
-
+// also the page includes a  MyButton that will direct users to other pages.
+// next, we use  MaterialCommunityIcons to import the required instagram photo image requested.
+// the page displays different content bassed on the values of pdfShowing && pdfPath
+// if both pdfShowing && pdfPath are true the PDFVIEW layout will appear on the homepage
+// if either one is false then a scrollview consisting of buttons will be shown on the homepage 
 
 const styles: { [key: string]: any } = StyleSheet.create({
+  pdfContainer: {
+    flex: 1, // Ensures the PDF viewer takes up the full screen
+    width: "100%", // Ensures it stretches to the full width of the screen
+    height: "100%", // Ensures it stretches to the full height of the screen
+  },
 
-  scrollViewContainer:{
+  scrollViewContainer: {
     flex: 1,
     backgroundColor: "#eda201",
   },
 
-
   container: {
     flex: 1,
     justifyContent: "flex-start",
-    
   },
 
   gifImage: {
@@ -120,6 +184,6 @@ const styles: { [key: string]: any } = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     justifyContent: "center",
-    marginTop: 30,
+    marginTop: 15,
   },
 });
